961eddef2c5e6d4079771c18c42adb86f43b7615
Ken Goddard <closerwalk@gmail.com>
2022-08-29 21:27:56 -0700

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,7 +11,7 @@ terraform {
 #}              
 provider "aws" {
   region     = "us-west-2"
-  access_key = ${AWS_ACCESS_KEY_ID}
-  secret_key = ${AWS_SECRET_ACCESS_KEY}
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
