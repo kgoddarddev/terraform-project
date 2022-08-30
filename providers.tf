47c368a9c19f230a8f2ff7fa1c5a1914290fa33c
Ken Goddard <closerwalk@gmail.com>
2022-08-29 21:18:36 -0700

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -5,8 +5,13 @@ terraform {
     }
   }
 }
+#provider "aws" {
+#  shared_credentials_files = ["/home/ec2-user/.aws/credentials"]
+#  profile    = "kgoddard"
+#}              
 provider "aws" {
-  shared_credentials_files = ["/home/ec2-user/.aws/credentials"]
-  profile                  = "kgoddard"
+  region     = "us-west-2"
+  access_key = AWS_ACCESS_KEY_ID
+  secret_key = AWS_SECRET_ACCESS_KEY
 }
 
