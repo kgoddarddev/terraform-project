eb9504546b92f4d4f55f252d92199a58249ee975
Ken Goddard <closerwalk@gmail.com>
2022-08-29 20:41:57 -0700

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -8,5 +8,5 @@ terraform {
 provider "aws" {
   region                  = "us-west-2"
   shared_credentials_file = "~/.aws/credentials"
-  profile                 = "kgoddard"
+  profile                 = "kmgoddard"
 }
