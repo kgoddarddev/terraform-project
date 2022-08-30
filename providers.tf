8c976973ed2e55d4945d0a49e7fa29451947e851
Ken Goddard <closerwalk@gmail.com>
2022-08-30 07:38:53 -0700

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -11,7 +11,7 @@ terraform {
 #  profile    = "ilab"
 #}
 provider "aws" {
-access_key = var.AWS_ACCESS_KEY_ID
-secret_key = var.AWS_SECRET_ACCESS_KEY
+access_key = "${params.AWS_ACCESS_KEY_ID}"
+secret_key = "${params.AWS_SECRET_ACCESS_KEY}"
 region     = "us-west-2"
 }
